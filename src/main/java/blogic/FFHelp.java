4d91718ac3ac923299f2c763e043d3a2f3398bd6
AKirillM <slunder.buntik@gmail.com>
2025-03-18 16:25:44 +0600

    Добавление расчета площади Параллелограмма

--- src/main/java/blogic/FFHelp.java
+++ src/main/java/blogic/FFHelp.java
@@ -5,4 +5,8 @@ public class FFHelp {
         double p = (a + b + c) / 2;
         return Math.sqrt(p *(p-a) * (p-b) * (p-c));
     }
+
+    public static double parallelogramArea(double a, double b, int angle){
+        return a*b*Math.sin(angle * Math.PI / 100 );
+    }
 }
